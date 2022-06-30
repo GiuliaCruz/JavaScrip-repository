2ac420846eab2ddbb728e2bcc9597a777005d724
GiuliaCruz <giuliacruzz@gmail.com>
2022-06-30 17:15:38 -0300

    Data_1

---
+++ Start/Data_1.js
@@ -0,0 +1,11 @@
+let name = "Pen";
+let  quantity = 10;
+let price = 6.4;
+let tax = 1.5;
+let finalPrice = price + tax;
+
+console.log(name);
+console.log(quantity);
+console.log(price);
+console.log(tax);
+console.log(finalPrice);

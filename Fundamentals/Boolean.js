27309a1fd150d2055c5e723c1b5124203a85b7ad
GiuliaCruz <giuliacruzz@gmail.com>
2022-07-21 18:25:03 -0300

    add to Fundamentals

---
+++ Fundamentals/Boolean.js
@@ -0,0 +1,22 @@
+let isActive = false
+console.log(isActive)
+
+isActive = true
+console.log(isActive)
+
+isActive = 1
+console.log(!!isActive)
+
+console.log("true...")
+console.log(!!3)
+console.log(!!-1)
+console.log(!!" ")
+console.log(!!{})
+console.log(!![])
+console.log(!!Infinity)
+
+console.log("false...")
+console.log(!!0)
+console.log(!!"")
+console.log(!!null)
+console.log(!!undefined)
---
+++ Fundamentals/Object.js
@@ -0,0 +1,13 @@
+const prod1 = {} // {} means object
+prod1.name = "Smartphone"
+prod1.price = 4998.90
+prod1["discount"] = 0.40
+
+console.log(prod1)
+
+const prod2 ={
+    name:"T-Shirt",
+    price: 79.90  
+    }
+
+console.log(prod2) 

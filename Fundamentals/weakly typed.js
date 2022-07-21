dccdf0205d5f8140c29cc33f9199dd761c4dc679
GiuliaCruz <giuliacruzz@gmail.com>
2022-07-21 15:26:50 -0300

    add to Fundamentals

---
+++ Fundamentals/weakly typed.js
@@ -0,0 +1,12 @@
+let whatever = "Cool"
+console.log(whatever)
+console.log(typeof whatever)
+
+whatever = 3.1516
+console.log(whatever)
+console.log(typeof whatever)
+
+//Avoid to use generic names or acronyms, like:
+let value = " "
+let number = 10
+let pin = false // Personal Identification Number... lol

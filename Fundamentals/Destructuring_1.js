d52d3331d92a25b370379efca7d22e15867d91a4
GiuliaCruz <giuliacruzz@gmail.com>
2022-07-26 18:14:23 -0300

    Add to Fundamentals

---
+++ Fundamentals/Destructuring_1.js
@@ -0,0 +1,11 @@
+const person = {
+    name: "Alex",
+    age: 27,
+    address: {
+        Street: "Whatever",
+        number: 1000
+    }
+}
+
+const { name , age } = person
+console.log(name, age)

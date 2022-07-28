f6d873741293c3a3ba36e379d93bee28db87d0d3
GiuliaCruz <giuliacruzz@gmail.com>
2022-07-28 15:28:49 -0300

    add to Fundamentals

---
+++ Fundamentals/Destructuring_3.js
@@ -0,0 +1,8 @@
+function rand({min = 0, max = 1000}) {
+    const value = Math.random() * (max - min) + min
+    return Math.floor(value)
+}
+
+console.log(rand({ max: 50, min: 40 }))
+console.log(rand(obj))
+console.log(rand({min: 955 }))
